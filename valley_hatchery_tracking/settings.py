cdd4387c731d35d45b140aa9591186d5802bb9e8
vts <visionarytechsolution24@gmail.com>
2025-06-03 00:01:50 +0600

    Update Database Style

--- valley_hatchery_tracking/settings.py
+++ valley_hatchery_tracking/settings.py
@@ -130,6 +130,26 @@ CORS_ALLOW_ALL_ORIGINS=True
 # Database
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
+# if DEBUG:
+#     DATABASES = {
+#         'default': {
+#             'ENGINE': 'django.db.backends.sqlite3',
+#             'NAME': BASE_DIR / 'db.sqlite3',
+#         }
+#     }
+# else:
+#     DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.postgresql_psycopg2',
+#         'NAME': 'trackingapi',
+#         'USER': 'trackinguser',
+#         'PASSWORD': 'trackingapi',
+#         'HOST': 'localhost',
+#         'PORT': '',
+#     }
+# }
+
+
 if DEBUG:
     DATABASES = {
         'default': {
@@ -139,15 +159,11 @@ if DEBUG:
     }
 else:
     DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'trackingapi',
-        'USER': 'trackinguser',
-        'PASSWORD': 'trackingapi',
-        'HOST': 'localhost',
-        'PORT': '',
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': BASE_DIR / 'db.sqlite3',
+        }
     }
-}
 
 
 # Password validation
